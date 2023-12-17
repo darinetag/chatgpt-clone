0219fd05191be4d554af77d08fb4d581ee99361a
Darine <tagdarinefatma@gmail.com>
2023-12-17 01:37:22 +0100

    firebase integration, add chats and links, delete chats

---
+++ chatgpt/app/chat/[id]/page.tsx
@@ -0,0 +1,8 @@
+function Chatpage() {
+  return <div>
+
+    
+  </div>;
+}
+
+export default Chatpage;
---
+++ chatgpt/firebase.ts
@@ -0,0 +1,19 @@
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrRHhcnhba4Vp7DHzLmmX3wV5j8RI4vJQ",
+  authDomain: "chatgpt-cloned.firebaseapp.com",
+  projectId: "chatgpt-cloned",
+  storageBucket: "chatgpt-cloned.appspot.com",
+  messagingSenderId: "819158927697",
+  appId: "1:819158927697:web:a5feffa4e87ed8b859eb70"
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {db};
